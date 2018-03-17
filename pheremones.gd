13310f32276b86b9bda19158c67e9386db79e5cf
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-03-17 16:58:40 +0200

    Fix pheremone trail, cleanup

--- pheremones.gd
+++ pheremones.gd
@@ -2,7 +2,7 @@ extends Line2D
 
 var point
 var target
-var startingPosition
+
 export(NodePath) var targetPath
 
 func _ready():
@@ -10,11 +10,12 @@ func _ready():
 	# Initialization here
 	target = get_node(targetPath)
 
-	startingPosition = target.position
-
 func _process(delta):
-	#global_position = Vector2(startingPosition)
+	global_position = Vector2(0,0)
 	global_rotation = 0
+	
+	# get point for current position of target
 	point = target.position
 	
+	# add new point to list
 	add_point(point)
