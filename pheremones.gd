f5efc2f12a5bcf8d187900ac211b1a181e54eb36
Brylie Christopher Oxley <brylie@gnumedia.org>
2018-03-17 18:53:00 +0200

    Make sure ant has traveled some distance before secreting pheremone; cleanup

--- pheremones.gd
+++ pheremones.gd
@@ -1,14 +1,13 @@
 extends Line2D
 
-var point
 var target
 
-export(NodePath) var targetPath
+export(NodePath) var target_path
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	target = get_node(targetPath)
+	target = get_node(target_path)
 
 func _process(delta):
 	# Make sure pheremone trail is properly aligned to world and ant
@@ -16,14 +15,29 @@ func _process(delta):
 	global_rotation = 0
 
 func _on_evaporation_timeout():
-	print("total points: ", get_point_count())
+	print('remove point')
 	# Remove oldest pheremone point
 	remove_point(0)
 
 
 func _on_secretion_timeout():
-	# get point for current position of target
-	point = target.position
+	# Get point for current position of target
+	var current_position = target.position
 	
-	# add new point to list
-	add_point(point)
+	# Get count of pheremone points
+	var pheremone_point_count = get_point_count()
+	
+	if (pheremone_point_count < 2):	
+		# Not enough pheremones, secrete some
+		add_point(current_position)
+	else:
+		# Get index for previous pheremone point (subtract one since array is zero indexed)	
+		var previous_pheremone_position = get_point_position(pheremone_point_count - 1)
+		
+		# Get travel distance from previous pheremone point to current position
+		var travel_distance = previous_pheremone_position.distance_to(current_position)
+		
+		# Make sure ant has traveled some distance before adding pheremone point
+		if (travel_distance > 10):
+			# Add new point to list
+			add_point(current_position)
